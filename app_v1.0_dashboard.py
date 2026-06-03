9c593ca23627c99f69635098ddf54fe8b4f2b474
Elliot Adams <safe0045@ox.ac.uk>
2026-06-03 14:10:47 +0100

    changed date inputs v1.13

--- app_v1.0_dashboard.py
+++ app_v1.0_dashboard.py
@@ -149,17 +149,17 @@ with st.expander("Employee Details", expanded=True):
         step=0.1
     )
 
-    employment_start_text = st.text_input(
+    employment_start_text = st.date_input(
         "Employment Start Date (DD/MM/YYYY)",
         value="01/09/2024"
     )
 
-    due_date_text = st.text_input(
+    due_date_text = st.date_input(
         "Expected Due Date (DD/MM/YYYY)",
         value="03/06/2026"
     )
 
-    leave_start_text = st.text_input(
+    leave_start_text = st.date_input(
         "Maternity Leave Start Date (DD/MM/YYYY)",
         value="11/06/2026"
     )
